3ba2d4b22c06583b59f502a2e12adcc04c466e5b
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-11-04 21:01:29 +0900

    hyehye

--- app/src/main/java/kr/hs/emirim/sumtan/user/Course.java
+++ app/src/main/java/kr/hs/emirim/sumtan/user/Course.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import kr.hs.emirim.sumtan.R;
 
-public class Course extends Fragment {
+public class Course extends Fragment{
+
+    private Button submission_Button;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+
+
         return inflater.inflate(R.layout.search_course,container,false);
 
     }
