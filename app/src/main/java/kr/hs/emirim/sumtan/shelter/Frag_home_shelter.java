5ce4c3916fe09bd1edb83d26810a443627b15a01
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-11-26 16:20:54 +0900

    광고 webview commit

--- app/src/main/java/kr/hs/emirim/sumtan/shelter/Frag_home_shelter.java
+++ app/src/main/java/kr/hs/emirim/sumtan/shelter/Frag_home_shelter.java
@@ -1,9 +1,18 @@
 package kr.hs.emirim.sumtan.shelter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +20,15 @@ import androidx.fragment.app.Fragment;
 
 import kr.hs.emirim.sumtan.R;
 
-public class Frag_home_shelter extends Fragment {
+public class Frag_home_shelter extends Fragment implements View.OnClickListener {
 
     private View view;
+    private ImageButton ad1_shelter;
+    private ImageButton ad2_shelter;
+    private ImageButton ad3_shelter;
+    private ImageButton ad4_shelter;
+    private Button btn_back;
+    private WebView wv;
 
     @Nullable
     @Override
@@ -21,6 +36,76 @@ public class Frag_home_shelter extends Fragment {
     {
         view = inflater.inflate(R.layout.activity_frag_home_shelter,container,false);
 
+        ad1_shelter=(ImageButton)view.findViewById(R.id.ad1_shelter);
+        ad2_shelter=(ImageButton)view.findViewById(R.id.ad2_shelter);
+        ad3_shelter=(ImageButton)view.findViewById(R.id.ad3_shelter);
+        ad4_shelter=(ImageButton)view.findViewById(R.id.ad4_shelter);
+        btn_back=(Button)view.findViewById(R.id.btn_back);
+        wv=view.findViewById(R.id.wv);
+
+        WebSettings webSettings = wv.getSettings(); //자바 스크립트 사용을 할 수 있도록 합니다.
+
+        webSettings.setJavaScriptEnabled(true); wv.setWebViewClient(new WebViewClient(){ //페이지 로딩이 끝나면 호출됩니다.
+            @Override public void onPageFinished(WebView view,String url){
+                Toast.makeText(getActivity(),"로딩 끝", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ad1_shelter.setOnClickListener(this);
+        ad2_shelter.setOnClickListener(this);
+        ad3_shelter.setOnClickListener(this);
+        ad4_shelter.setOnClickListener(this);
+
+        wv.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction()!=KeyEvent.ACTION_DOWN)
+                    return true;
+
+                if(keyCode==KeyEvent.KEYCODE_BACK){
+                    if(wv.canGoBack()){
+                        wv.goBack();
+                    }else{
+                        wv.setVisibility(View.INVISIBLE);
+                    }
+                }
+
+                return true;
+            }
+        });
+
         return view;
     }
+
+    @Override
+    public void onClick(View v) {
+        String address;
+        wv.setVisibility(View.VISIBLE);
+        btn_back.setVisibility(View.VISIBLE);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wv.setVisibility(View.INVISIBLE);
+            }
+        });
+        switch (v.getId()){
+            case R.id.ad1_shelter:
+                address="https://www.animals.or.kr/report/print/53990";
+                wv.loadUrl(address);
+                break;
+            case R.id.ad2_shelter:
+                address="https://secure.donus.org/animals/pay/step1_direct?dontype=P10101&period=pledge&price=20000&_ga=2.215230295.1369925755.1606368633-1914975462.1605673261&_gac=1.226724719.1606369265.CjwKCAiAnvj9BRA4EiwAuUMDf-BFeMN0KOnZN1HIghRQo8-NE-5ICWHjNqC3-2WaEKOgfrlLCi8bnRoCAucQAvD_BwE";
+                wv.loadUrl(address);
+                break;
+            case R.id.ad3_shelter:
+                address="https://www.animals.or.kr/report/print/52229";
+                wv.loadUrl(address);
+                break;
+            case R.id.ad4_shelter:
+                address="https://www.animals.or.kr/report/print/39803";
+                wv.loadUrl(address);
+                break;
+        }
+    }
+
 }
--- app/src/main/java/kr/hs/emirim/sumtan/user/Frag_home_user.java
+++ app/src/main/java/kr/hs/emirim/sumtan/user/Frag_home_user.java
@@ -1,9 +1,19 @@
 package kr.hs.emirim.sumtan.user;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +21,16 @@ import androidx.fragment.app.Fragment;
 
 import kr.hs.emirim.sumtan.R;
 
-public class Frag_home_user extends Fragment {
+public class Frag_home_user extends Fragment implements View.OnClickListener {
 
     private View view;
+    private ImageButton ad1_user;
+    private ImageButton ad2_user;
+    private ImageButton ad3_user;
+    private ImageButton ad4_user;
+    private ImageButton ad5_user;
+    private Button btn_back;
+    private WebView wv;
 
     @Nullable
     @Override
@@ -21,6 +38,83 @@ public class Frag_home_user extends Fragment {
     {
         view = inflater.inflate(R.layout.activity_frag_home_user,container,false);
 
+        ad1_user=(ImageButton)view.findViewById(R.id.ad1_user);
+        ad2_user=(ImageButton)view.findViewById(R.id.ad2_user);
+        ad3_user=(ImageButton)view.findViewById(R.id.ad3_user);
+        ad4_user=(ImageButton)view.findViewById(R.id.ad4_user);
+        ad5_user=(ImageButton)view.findViewById(R.id.ad5_user);
+        btn_back=(Button)view.findViewById(R.id.btn_back);
+
+        wv=view.findViewById(R.id.wv);
+
+        WebSettings webSettings = wv.getSettings(); //자바 스크립트 사용을 할 수 있도록 합니다.
+
+        webSettings.setJavaScriptEnabled(true); wv.setWebViewClient(new WebViewClient(){ //페이지 로딩이 끝나면 호출됩니다.
+            @Override public void onPageFinished(WebView view,String url){
+                Toast.makeText(getActivity(),"로딩 끝", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        wv.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction()!=KeyEvent.ACTION_DOWN)
+                    return true;
+
+                if(keyCode==KeyEvent.KEYCODE_BACK){
+                    if(wv.canGoBack()){
+                        wv.goBack();
+                    }else{
+                        wv.setVisibility(View.INVISIBLE);
+                    }
+                }
+
+                return true;
+            }
+        });
+
+        ad1_user.setOnClickListener(this);
+        ad2_user.setOnClickListener(this);
+        ad3_user.setOnClickListener(this);
+        ad4_user.setOnClickListener(this);
+        ad5_user.setOnClickListener(this);
+
         return view;
     }
+
+    @Override
+    public void onClick(View v) {
+        String address;
+        wv.setVisibility(View.VISIBLE);
+        btn_back.setVisibility(View.VISIBLE);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wv.setVisibility(View.INVISIBLE);
+            }
+        });
+        switch (v.getId()){
+            case R.id.ad1_user:
+                address="https://ekara.org/thebom/article/read/13594";
+                wv.loadUrl(address);
+                break;
+            case R.id.ad2_user:
+                address="https://www.ekara.org/thebom/article/read/13672";
+                wv.loadUrl(address);
+                break;
+            case R.id.ad3_user:
+                address="https://www.youtube.com/watch?v=5NE4n0WsYh0";
+                wv.loadUrl(address);
+                break;
+            case R.id.ad4_user:
+                address="https://www.youtube.com/watch?v=cyAE2oht71w";
+                wv.loadUrl(address);
+                break;
+            case R.id.ad5_user:
+                address="https://www.animals.or.kr/home/notice/53563";
+                wv.loadUrl(address);
+                break;
+        }
+    }
+
 }
