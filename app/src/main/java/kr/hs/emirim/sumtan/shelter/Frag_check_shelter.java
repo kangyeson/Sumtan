6b91cb51e900b91158dd8e2d2cd590848a032788
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-04-21 20:15:34 +0900

    Fix frag error

--- app/src/main/java/kr/hs/emirim/sumtan/shelter/Frag_check_shelter.java
+++ app/src/main/java/kr/hs/emirim/sumtan/shelter/Frag_check_shelter.java
@@ -1,6 +1,6 @@
 package kr.hs.emirim.sumtan.shelter;
 
-import android.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
